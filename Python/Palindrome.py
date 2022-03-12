c4be40dd15724714aa0f4daa579604c6e0d5963d
Shayak Rakshit <85937567+iamgithum@users.noreply.github.com>
2022-03-12 22:36:16 +0530

    Create Palindrome.py

---
+++ Python/Palindrome.py
@@ -0,0 +1,12 @@
+# Palindrome Number
+var = input("Enter a number: ")
+p = 1
+i = 1
+while i <= int(var):
+    print(i, end=" ")
+    i = i + 1
+
+i = int(var) -1
+while i > 0 :
+    print(i, end=" ")
+    i = i - 1
